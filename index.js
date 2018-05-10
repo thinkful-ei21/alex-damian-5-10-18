3fcf012f3e2d38c2ec2bd9b51e0f99f0714ed20c
Damian Marius <damian.mm@live.com>
2018-05-10 12:44:53 -0700

    added deletion and checking of list items

--- index.js
+++ index.js
@@ -23,4 +23,12 @@ function main(){
     });
 }
 
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+
 $(main());
